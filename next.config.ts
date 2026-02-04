d5a05fed97d7ecd4166cf8512b1027028188f3b1
Bart De Block <deblock.bart@gmail.com>
2026-02-04 08:45:46 +0100

    chore: redirect www to apex

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.buitendrogen.be" }],
+        destination: "https://buitendrogen.be/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
